aa3e5b7d7bcc0c16975f80b00e6cbd45f3b8876f
andyMonterrubio <andy.mont18@gmail.com>
2020-05-01 19:14:58 -0500

    adding views and static files

--- weblog/views.py
+++ weblog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from weblog.models import Post, Comment, Category
 
 # Create your views here.
+#request: Django provides the object 
+def blog_index(request):
+    #return last 10 elements
+    posts = Post.objects.filter(is_published=True).order_by("-date")
+
+    return render(
+        request, 
+        "index.html", 
+        context = { "posts": posts }
+    )
+
